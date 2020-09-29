1ffc5845961150b458c22508d649385495d778fb
lianggx <lianggx@foxmail.com>
2020-09-29 10:28:38 +0800

    3.0.7

--- examples/Pgsql/Program.cs
+++ examples/Pgsql/Program.cs
@@ -10,8 +10,6 @@ namespace Pgsql
         static void Main(string[] args)
         {
             PgsqlDbContext dbContext = new PgsqlDbContext(new StagingOptions("Pgsql", "Host=127.0.0.1;Port=5432;Username=postgres;Password=postgres;Database=mystaging;"));
-            var art = dbContext.Article.Select.Where(f => f.id == "5ee1c721b5ee483998000001").ToOne();
-
             new ContextTest().Start();
 
             Console.ReadKey();
